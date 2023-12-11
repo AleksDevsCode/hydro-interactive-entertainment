b1195ba61e7b853f53c5f3ecfe337f4868fef612
BuildTools <unconfigured@null.spigotmc.org>
2023-12-11 22:31:07 +0000

    web

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
     return (
@@ -7,15 +9,15 @@ export default function Home() {
             <div className="relative">
                 <div className="outer-container text-center">
                     <div className="gradient-border">
-                        <img src="./logo.png" alt="Hydro Interactive Entertainment Logo" className="w-full h-full" />
+                        <Image width={256} height={256} src="/logo.png" alt="Hydro Interactive Entertainment Logo" />
                     </div>
                     <br />
                 </div>
             </div>
             <h1 className="text-6xl font-medium mt-4">Hydro Interactive Entertainment</h1>
             <br />
-            <a href="/play">            <button className="text-black text-2xl bg-white rounded-full w-[300px] h-[50px] hover:translate-y-[-10px] transition duration-300 ease-in-out">Play</button>
-            </a>
+            <Link href="/play">            <button className="text-black text-2xl bg-white rounded-full w-[300px] h-[50px] hover:translate-y-[-10px] transition duration-300 ease-in-out">Play</button>
+            </Link>
         </div>
     );
 }
